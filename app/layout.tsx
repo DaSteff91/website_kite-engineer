cf73da12714b2666dcc873ad8eb2f23b687f65b6
DaSteff91 <stefan.merthan@yahoo.de>
2025-05-20 21:43:37 +0200

    Testing new workflow for development branch

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
       type: "image/x-icon",
     },
   ],
-  keywords: "Kiteboarding, Engineering",
+  keywords: "Kiteboarding, Engineering, Testing",
 };
 
 export default function RootLayout({
