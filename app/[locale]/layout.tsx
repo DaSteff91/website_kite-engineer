ac4f28fffca423a456cf43fb50340bf05db4fbed
Da_Steff <159436322+DaSteff91@users.noreply.github.com>
2025-05-25 17:01:39 +0200

    Implement internationalization

---
+++ app/[locale]/layout.tsx
@@ -0,0 +1,47 @@
+import { Inter } from "next/font/google";
+import { notFound } from "next/navigation";
+import { NextIntlClientProvider } from "next-intl";
+import { ThemeProvider } from "@/providers/theme-provider";
+import { ClientLayoutWrapper } from "../ClientLayoutWrapper";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export function generateStaticParams() {
+  return [{ locale: "en" }, { locale: "de" }, { locale: "pt" }];
+}
+
+export default async function LocaleLayout({
+  children,
+  params: { locale },
+}: {
+  children: React.ReactNode;
+  params: { locale: string };
+}) {
+  let messages;
+  try {
+    messages = (await import(`../../messages/${locale}.json`)).default;
+  } catch (error) {
+    notFound();
+  }
+
+  return (
+    <html lang={locale} suppressHydrationWarning>
+      <body className={inter.className}>
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <ClientLayoutWrapper>
+              <div className="min-h-screen flex flex-col">
+                <main className="flex-grow">{children}</main>
+              </div>
+            </ClientLayoutWrapper>
+          </ThemeProvider>
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
