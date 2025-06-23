33aee527c46b8bab99e234f111ceba7de0d00b1a
DaSteff91 <stefan.merthan@yahoo.de>
2025-06-23 18:14:29 +0200

    feat: implement auto reply via email from contact form

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -13,7 +13,7 @@ export async function POST(req: NextRequest) {
       );
     }
 
-    // Configure the transporter with STARTTLS
+    // Configure the mail sending transporter
     const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST!,
       port: Number(process.env.SMTP_PORT) || 587,
@@ -28,7 +28,7 @@ export async function POST(req: NextRequest) {
       },
     });
 
-    // Send the email
+    // Send the email to myself
     await sendEmail({
       //from: process.env.SMTP_USER!,
       to: process.env.CONTACT_EMAIL!,
@@ -36,6 +36,28 @@ export async function POST(req: NextRequest) {
       text: message,
       replyTo: email,
     });
+
+    await sendEmail({
+      //from: process.env.SMTP_USER!,
+      to: email,
+      subject: `RE: "${subject}"`,
+      html: `
+        <p>Hello ${name},</p>
+        <p>Thanks for reaching out to me – I’ve received your message and will reply as soon as possible.</p>
+        <p>Schön, dass du mir schreibst – ich habe deine Nachricht erhalten und werde schnellstmöglich antworten.</p>
+        <p>Obrigado por entrar em contato comigo – eu recebi sua mensagem e responderei o mais cedo que possível.</p>
+        <p>Steff</p>
+        <p><strong>Kite-Engineer by Stefan Merthan</strong></p>
+        <p>
+          <a href="https://www.kite-engineer.de" target="_blank">www.kite-engineer.de</a><br/>
+          <a href="mailto:stefan@kite-engineer.de">stefan@kite-engineer.de</a>
+        </p>
+        <hr />
+
+        <p><strong>Original</strong><br/>${message}</p>
+      `,
+      replyTo: process.env.CONTACT_EMAIL!,
+    })
 
     return NextResponse.json(
       { message: 'Message sent successfully.' },
